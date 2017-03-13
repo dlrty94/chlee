8ed2b350a4142c997a11ef1a5f07bd40752b9270
dlrty94 <dlckdgus2004@naver.com>
2017-03-13 17:05:42 +0900

    commit commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var webpack = require('webpack');
+module.exports = {
+    entry: './src/index.js',
+
+    output: {
+        path: __dirname + '/public/',
+        filename: 'bundle.js'
+    },
+
+    devServer: {
+        inline: true,
+        port: 7777,
+        contentBase: __dirname + '/public/'
+    },
+
+    module: {
+            loaders: [
+                {
+                    test: /\.js$/,
+                    loaders: ['react-hot','babel?' + JSON.stringify({
+                          cacheDirectory: true,
+                          presets: ['es2015', 'react']
+                    })],
+                    exclude: /node_modules/,
+
+                }
+            ]
+        },
+        plugins : [
+          new webpack.HotModuleReplacementPlugin()
+        ]
+
+};
